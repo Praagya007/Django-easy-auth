8516ae59134d8339b2c6b0bab942f7a8cb83e9a4
Praagya <praagyadevkota@gmail.com>
2026-07-24 07:29:02 +0545

    Django environ installed, and initialized env variables. Note: It is hidden in VSCode to prevent Copilot/Code Assist from reading it

--- config/settings/base.py
+++ config/settings/base.py
@@ -11,21 +11,31 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+#Initialize environ and set type defaults.
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, True),
+    ALLOWED_HOSTS=(list, [])
+)
+
+#Read the .env from your base directory
+environ.Env.read_env(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dc=_oikm70)q3!k%y9*3a4!onqjjj(ts@!g3n!(az20&1&ocjm'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production! 
-DEBUG = True
+DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env('ALLOWED_HOSTS')
 
 
 # Application definition
