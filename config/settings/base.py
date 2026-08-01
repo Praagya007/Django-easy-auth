b31ac088613b31b26f3ac5bc4b8af5d924996ece
Praagya <praagyadevkota@gmail.com>
2026-08-01 08:46:22 +0545

    Allowed hosts for Django currently, including local ones, internal docker ones and IPv6.

--- config/settings/base.py
+++ config/settings/base.py
@@ -35,7 +35,9 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production! 
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = env('ALLOWED_HOSTS')
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', 
+    default=['localhost', '127.0.0.1', '[::1]', 'host.docker.internal', 'django']
+)  # Split the comma-separated list of allowed hosts
 
 
 # Application definition
